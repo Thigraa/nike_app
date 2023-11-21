045bfce072fdf0a403707bce2a14bdbed8993833
Joan Lecha <lechajoan@gmail.com>
2023-11-21 11:42:30 +0100

    App Commit

---
+++ lib/src/widgets/custom_widgets.dart
@@ -0,0 +1,5 @@
+export 'package:nike_app/src/widgets/custom_appbar.dart';
+export 'package:nike_app/src/widgets/zapato_size.dart';
+export 'package:nike_app/src/widgets/zapato_desc.dart';
+export 'package:nike_app/src/widgets/agregar_carrito.dart';
+export 'package:nike_app/src/widgets/boton_naranja.dart';
